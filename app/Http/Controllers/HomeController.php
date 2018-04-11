705aefd147b0d2aa15a7f8504b0d62aaef17f693
Gman1996 <dayoagbolade@gmail.com>
2018-04-11 21:00:21 +0100

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('pages.index');
+    }
+
+    public function profile()
+    {
+        return view('pages.profile');
+    }
+
+    public function search()
+    {
+        return view('pages.search');
+    }
+}
---
+++ resources/assets/js/components/Search.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>Search compoment here</h1>
+        <vue-google-autocomplete
+                ref="address"
+                id="map"
+                classname="form-control"
+                placeholder="Please type your address"
+                v-on:placechanged="getAddressData"
+                country="['us','ng']"
+        >
+        </vue-google-autocomplete>
+    </div>
+</template>
+<script>
+    import VueGoogleAutocomplete from 'vue-google-autocomplete'
+    export default {
+       components: { VueGoogleAutocomplete },
+        data(){
+            return {
+                address: ''
+            }
+        },
+        methods: {
+            /**
+             * When the location found
+             * @param {Object} addressData Data of the found location
+             * @param {Object} placeResultData PlaceResult object
+             * @param {String} id Input container ID
+             */
+            getAddressData: function (addressData, placeResultData, id) {
+                this.address = addressData;
+                console.log(placeResultData)
+            },
+        },
+        created(){
+
+        },
+        mounted(){
+            // To demonstrate functionality of exposed component functions
+            // Here we make focus on the user input
+            this.$refs.address.focus();
+        },
+        computed: {
+
+        }
+    }
+</script>
+<style scoped>
+
+</style>
